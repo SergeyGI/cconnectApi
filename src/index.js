0153a3cc97114648adf36d320e346e41dd256262
gsi <sergey.1c@bk.ru>
2020-03-30 11:32:28 +0300

    Add security helmet

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 /** @format */
 
 const express = require('express')
+const helmet = require('helmet')
 
 const router = require('./app/router')
 const port = 3000
@@ -10,4 +11,5 @@ app.listen(port, () => {
   console.log(`API started to port ${port}`)
 })
 
+app.use(helmet())
 app.use('/', router)
