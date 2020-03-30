03eb1568bd0ee1174154e3490aeebb58c7ce1afa
gsi <sergey.1c@bk.ru>
2020-03-30 17:22:16 +0300

    Add docker mongodb connection

--- src/index.js
+++ src/index.js
@@ -2,13 +2,21 @@
 
 const express = require('express')
 const helmet = require('helmet')
+const MongoClient = require('mongodb').MongoClient
 
+const optionsMongo = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}
+const client = new MongoClient('mongodb://192.168.99.100:27017', optionsMongo)
 const router = require('./app/router')
 const port = 3000
 const app = express()
 
 app.listen(port, () => {
-  console.log(`API started to port ${port}`)
+  client.connect(err => {
+    console.log(err ? err : `API started to port ${port}`)
+  })
 })
 
 app.use(helmet())
