d18882917cafe4e6cbec34bc15abb894ff6514f1
gsi <sergey.1c@bk.ru>
2020-04-14 10:38:24 +0300

    Update model Hardware

--- src/app/models/hardware.js
+++ src/app/models/hardware.js
@@ -3,41 +3,36 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
-const proper = require('../validate/ip')
+const validIp = require('../validate/ip')
 
 const HardwareSchema = new Schema({
-  name: {
-    type: String,
-    required: [true, 'Введите имя объекта'],
-  },
+  name: { type: String },
   vendor: { type: String },
   model: { type: String },
-  ipAddress: {
-    type: String,
-    required: [true, 'Введите IP адрес'],
-  },
-  radiusSecret: {
-    type: String,
-    required: [true, 'Обязательное поле <radiusSecret>'],
-  },
+  ipAddress: { type: String },
+  radiusSecret: { type: String },
   installationAddress: { type: String },
   status: { type: Boolean, default: true },
   createdDate: { type: Date, default: Date.now },
-  updateDate: { type: Date, default: Date.now },
 })
 
-HardwareSchema.path('name').validate(name => {
-  return name.length
-}, 'Введите имя объекта')
+HardwareSchema.path('name')
+  .required(true, 'Введите имя объекта')
+  .validate(name => {
+    return name.length
+  }, 'Введите имя объекта')
 
-HardwareSchema.path('ipAddress').validate(ip => {
-  // return ip.length && proper(ip)
-  return proper(ip)
-}, 'Введите корректный IP адрес')
+HardwareSchema.path('ipAddress')
+  .required(true, 'Введите IP адрес')
+  .validate(ip => {
+    return ip.length && validIp(ip)
+  }, 'Введите корректный IP адрес')
 
-HardwareSchema.path('radiusSecret').validate(radiusSecret => {
-  return radiusSecret.length > 5
-}, 'Секретный ключ должен содержать больше 5 символов')
+HardwareSchema.path('radiusSecret')
+  .required(true, 'Введите секретный ключ')
+  .validate(radiusSecret => {
+    return radiusSecret.length > 5
+  }, 'Секретный ключ должен содержать больше 5 символов')
 
 // При обновлении документа увеличиваем его версию
 HardwareSchema.pre('findOneAndUpdate', function () {
